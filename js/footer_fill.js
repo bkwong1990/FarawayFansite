12f4054048ea7c7b472227e0f95f965a09fe261b
Bradley Wong <bkwong1990@gmail.com>
2018-07-24 23:15:24 -0700

    Added Github link

--- js/footer_fill.js
+++ js/footer_fill.js
@@ -3,6 +3,7 @@ document.getElementsByTagName("footer")[0].innerHTML =
 <p>Site made by Bradley Wong w/ Bootstrap 4 framework.</p>
 <p>Contact: bkwong1990 at gmail dot com</p>
 <p><a href="https://codepen.io/bkwong1990/">My Codepen page</a></p>
+<p><a href="https://github.com/bkwong1990/FarawayFansite">Faraway Fansite's Github Repo</a></p>
 </section>
 <section class="col-12 col-md-6">
 <h3>Special thanks to:</h3>

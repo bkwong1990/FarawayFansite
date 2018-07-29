0b7c45ff82925164088d2a27eed2806cfe0b9bb7
Bradley Wong <bkwong1990@gmail.com>
2018-07-28 18:06:20 -0700

    Used bullet list

--- js/footer_fill.js
+++ js/footer_fill.js
@@ -1,10 +1,12 @@
 document.getElementsByTagName("footer")[0].innerHTML =
 `<section class="col-12 col-md-6">
 <h3>About me</h3>
-<p>Site made by Bradley Wong w/ Bootstrap 4 framework.</p>
-<p>Contact: bkwong1990 at gmail dot com</p>
-<p><a href="https://codepen.io/bkwong1990/">My Codepen page</a></p>
-<p><a href="https://github.com/bkwong1990/FarawayFansite">Faraway Fansite's Github Repo</a></p>
+<ul>
+<li>Site made by Bradley Wong w/ Bootstrap 4 framework.</li>
+<li>Contact: bkwong1990 at gmail dot com</li>
+<li><a href="https://codepen.io/bkwong1990/">My Codepen page</a></li>
+<li><a href="https://github.com/bkwong1990/FarawayFansite">Faraway Fansite's Github Repo</a></li>
+</ul>
 </section>
 <section class="col-12 col-md-6">
 <h3>Special thanks to:</h3>

f236a25bd1512190167df0012b6977a46603e434
Bradley Wong <bkwong1990@gmail.com>
2018-09-14 15:59:13 -0700

    Add border to title h1

--- js/time.js
+++ js/time.js
@@ -5,25 +5,13 @@ function styleByTime(){
     body{
       background-image: url("img/continent_day.jpg");
     }
-
-    body > h1 {
-      color: brown;
-      text-shadow: 2px 2px 4px gray;
-      text-align: center;
-    }
     `;
   }
   else {
     document.getElementById("day_night").innerHTML = `
     body{
       background-image: url("img/continent_night.jpg");
-    }
-
-    #title {
-      color: white;
-      text-shadow: 2px 2px 4px gray;
-      text-align: center;
-    }
+    }   
     `;
   }
 }
